8db5c16268ddf98c9f62f907ff55e287e72be0f7
Chelsey Wang <chelseyyywang@gmail.com>
2020-01-14 22:37:49 -0800

    Updates

---
+++ precache-manifest.3ddac95b1f0f37e95155ed96ad382c16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a31e8d007a286d37574b3b34862411",
+    "url": "/rssla/index.html"
+  },
+  {
+    "revision": "7c2ded11d34a03d1cb9a",
+    "url": "/rssla/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4f2d4a2f0e7d88e112ef",
+    "url": "/rssla/static/js/2.2a25272c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rssla/static/js/2.2a25272c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7c2ded11d34a03d1cb9a",
+    "url": "/rssla/static/js/main.2e9f858b.chunk.js"
+  },
+  {
+    "revision": "2c29b599a70590db17db",
+    "url": "/rssla/static/js/runtime-main.a59e55d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rssla/static/media/logo.5d5d9eef.svg"
+  }
+]);
