d6d0efd8ae57d83efede3b84a2b2c5221859d990
Chelsey Wang <chelseyyywang@gmail.com>
2020-01-15 21:46:55 -0800

    omfg

--- src/index.js
+++ src/index.js
@@ -8,19 +8,19 @@ import Board from "./pages/Board";
 import Programs from "./pages/Programs";
 import Prospectives from "./pages/Prospectives";
 
-// const routing = (
-//     <Router basename={process.env.PUBLIC_URL}>
-//         <Switch>
-//             <Route exact path="/" component={App} />
-//             <Route exact path="/board" component={ Board } />
-//             <Route exact path="/programs" component={ Programs } />
-//             <Route exact path="/prospectives" component={ Prospectives } />
-//         </Switch>
-//     </Router>
-// )
+const routing = (
+    <Router basename={process.env.PUBLIC_URL}>
+        <Switch>
+            <Route exact path="/" component={App} />
+            <Route exact path="/board" component={ Board } />
+            <Route exact path="/programs" component={ Programs } />
+            <Route exact path="/prospectives" component={ Prospectives } />
+        </Switch>
+    </Router>
+)
 
-// ReactDOM.render(routing, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById('root'));
 
-ReactDOM.render(<App/>, document.getElementById('root')); 
+// ReactDOM.render(<App/>, document.getElementById('root')); 
 
 serviceWorker.unregister();
